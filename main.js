90313cb45daa890a7966f122d1436e2cc8c8e131
Vaskka <1139851358@qq.com>
2018-12-14 16:29:53 +0800

    change readme

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 3000,
+    height: 2000,
+    resizable: true,
+    maximizable: true
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
